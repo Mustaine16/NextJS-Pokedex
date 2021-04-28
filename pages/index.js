e3c038edf2fab9d298734dffc41049f7610244e3
Eze Dominguez <ezequieldominguezcp11@gmail.com>
2021-04-28 18:54:16 -0300

    vercel deploy (fix typo)

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 // import { useEffect } from 'react'
 import Image from 'next/image'
-import Link from 'next/Link'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home({ pokemons }) {
